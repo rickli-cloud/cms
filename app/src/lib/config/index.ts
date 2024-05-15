b729a9f14d30e474ba9e8d66ace051e9b92b57bc
needForBeans <lucamrickli@gmail.com>
2024-05-15 13:53:42 +0200

    types fix

--- app/src/lib/config/index.ts
+++ app/src/lib/config/index.ts
@@ -1,12 +1,27 @@
 import { z } from "zod";
 import backend from "./backend";
 import storage from "./storage";
-import collection from "./collection";
-
-export type * from "./index.d";
+import collection, {
+  type File,
+  type Folder,
+  type frontmatter,
+} from "./collection";
 
 export const config = z.object({
   collections: z.array(collection),
   storage: storage.default({}),
   backend,
 });
+
+export namespace Config {
+  export type Full = z.infer<typeof config>;
+  export type Backend = z.infer<typeof backend>;
+  export type Storage = z.infer<typeof storage>;
+
+  export namespace Collection {
+    export type File = z.infer<typeof File>;
+    export type Folder = z.infer<typeof Folder>;
+    export type Union = z.infer<typeof collection>;
+    export type Frontmatter = z.infer<typeof frontmatter>;
+  }
+}
